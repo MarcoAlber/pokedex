5335c859635f7d5c7d5e7fd137ec12803702b96e
Marco Alber <m.alber@onlinehome.de>
2023-02-11 18:52:22 +0100

    clean code+ load pokemon in json

--- script.js
+++ script.js
@@ -1,88 +1,78 @@
-let currentPokemon;
-let showStatsPokemon;
+let loadedPokemon = [''];
 
-async function showPokemon() {
+async function loadPokemon() {
     for (i = 1; i < 1009; i++) {
         let urlPokemon = `https://pokeapi.co/api/v2/pokemon-form/${i}/`;
         let responsePokemon = await fetch(urlPokemon);
-        currentPokemon = await responsePokemon.json();
+        let currentPokemon = await responsePokemon.json();
         let pokemonName = currentPokemon['pokemon']['name'];
         let urlPokemonWithStats = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
         let responsePokemonWithStats = await fetch(urlPokemonWithStats);
-        showStatsPokemon = await responsePokemonWithStats.json();
-        let showPokemonNumber = showStatsPokemon['id'];
-        let pokemonNameBigLetter = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-        let showPokemonImage = showStatsPokemon['sprites']['other']['official-artwork']['front_default'];
-        let pokemonType = showStatsPokemon['types']['0']['type']['name'];
-        let pokemonTypeGrass = pokemonType.includes("grass");
-        let pokemonTypeFire = pokemonType.includes("fire");
-        let pokemonTypeWater = pokemonType.includes("water");
-        let pokemonTypeBug = pokemonType.includes("bug");
-        let pokemonTypeNormal = pokemonType.includes("normal");
-        let pokemonTypePoison = pokemonType.includes("poison");
-        let pokemonTypeElectric = pokemonType.includes("electric");
-        let pokemonTypeGround = pokemonType.includes("ground");
-        let pokemonTypeFairy = pokemonType.includes("fairy");
-        let pokemonTypeFighting = pokemonType.includes("fighting");
-        let pokemonTypePsychic = pokemonType.includes("psychic");
-        let pokemonTypeRock = pokemonType.includes("rock");
-        let pokemonTypeGhost = pokemonType.includes("ghost");
-        let pokemonTypeIce = pokemonType.includes("ice");
-        let pokemonTypeDragon = pokemonType.includes("dragon");
-        let pokemonTypeDark = pokemonType.includes("dark");
-        let pokemonTypeSteel = pokemonType.includes("steel");
-        let pokemonTypeFlying = pokemonType.includes("flying");
-
-        if (showStatsPokemon['types'].length > 1) {
-            document.getElementById('containerWithPokemon').innerHTML += `
-            <div onclick="openPokemonCard(${i})" id="pokemonCard${i}" class="pokemonCard">
-                <span class="pokemonNumber"><b>${showPokemonNumber}</b></span>
-                <h1 id="pokemonNames">${pokemonNameBigLetter}</h1>
-                <img loading="lazy" id="pokemonImage" src= "${showPokemonImage}">
-                <div id="pokemonTypeContainer" class="pokemonTypeContainer">
-                    <span class="pokemonType">${pokemonType}</span>
-                    <span class="pokemonType">${showStatsPokemon['types']['1']['type']['name']}</span>
-                </div>
-                <div class="background-image-Card">
-                </div>
-            </div>`;
-        }
-
-        else {
-            document.getElementById('containerWithPokemon').innerHTML += `
-        <div onclick="openPokemonCard(${i})" id="pokemonCard${i}" class="pokemonCard">
-                <span class="pokemonNumber"><b>${showPokemonNumber}</b></span>
-                <h1 id="pokemonNames">${pokemonNameBigLetter}</h1>
-                <img loading="lazy" id="pokemonImage" src= "${showPokemonImage}">
-                <div id="pokemonTypeContainer" class="pokemonTypeContainer">
-                    <span class="pokemonType">${pokemonType}</span>
-                </div>
-                <div class="background-image-Card">
-                </div>
-        </div>`;
-        }
-
-        if (pokemonTypeGrass) { document.getElementById(`pokemonCard${i}`).classList.add('bg-grass') }
-        else if (pokemonTypeFire) { document.getElementById(`pokemonCard${i}`).classList.add('bg-fire') }
-        else if (pokemonTypeWater) { document.getElementById(`pokemonCard${i}`).classList.add('bg-water') }
-        else if (pokemonTypeBug) { document.getElementById(`pokemonCard${i}`).classList.add('bg-bug') }
-        else if (pokemonTypeNormal) { document.getElementById(`pokemonCard${i}`).classList.add('bg-normal') }
-        else if (pokemonTypePoison) { document.getElementById(`pokemonCard${i}`).classList.add('bg-poison') }
-        else if (pokemonTypeElectric) { document.getElementById(`pokemonCard${i}`).classList.add('bg-electric') }
-        else if (pokemonTypeGround) { document.getElementById(`pokemonCard${i}`).classList.add('bg-ground') }
-        else if (pokemonTypeFairy) { document.getElementById(`pokemonCard${i}`).classList.add('bg-fairy') }
-        else if (pokemonTypeFighting) { document.getElementById(`pokemonCard${i}`).classList.add('bg-fighting') }
-        else if (pokemonTypePsychic) { document.getElementById(`pokemonCard${i}`).classList.add('bg-psychic') }
-        else if (pokemonTypeRock) { document.getElementById(`pokemonCard${i}`).classList.add('bg-rock') }
-        else if (pokemonTypeGhost) { document.getElementById(`pokemonCard${i}`).classList.add('bg-ghost') }
-        else if (pokemonTypeIce) { document.getElementById(`pokemonCard${i}`).classList.add('bg-ice') }
-        else if (pokemonTypeDragon) { document.getElementById(`pokemonCard${i}`).classList.add('bg-dragon') }
-        else if (pokemonTypeDark) { document.getElementById(`pokemonCard${i}`).classList.add('bg-dark') }
-        else if (pokemonTypeSteel) { document.getElementById(`pokemonCard${i}`).classList.add('bg-steel') }
-        else if (pokemonTypeFlying) { document.getElementById(`pokemonCard${i}`).classList.add('bg-flying') }
+        let showStatsPokemon = await responsePokemonWithStats.json();
+        loadedPokemon.push(showStatsPokemon);
+        showPokemon(i);
     }
 }
 
-function openPokemonCard(i) {
+function showPokemon(i) {
+    let showPokemonNumber = loadedPokemon[i]['id'];
+    let pokemonNameBigLetter = loadedPokemon[i]['name'].charAt(0).toUpperCase() + loadedPokemon[i]['name'].slice(1);
+    let showPokemonImage = loadedPokemon[i]['sprites']['other']['official-artwork']['front_default'];
+    let pokemonType = loadedPokemon[i]['types']['0']['type']['name'];
 
+    if (loadedPokemon[i]['types'].length > 1) {
+        document.getElementById('containerWithPokemon').innerHTML += typeIsTwoTemplate(i, showPokemonNumber, pokemonNameBigLetter, showPokemonImage, pokemonType);
+    }
+    else {
+        document.getElementById('containerWithPokemon').innerHTML += typeIsOneTemplate(i, showPokemonNumber, pokemonNameBigLetter, showPokemonImage, pokemonType);
+    }
+    changeBackgroundColor(pokemonType);
+}
+
+function typeIsOneTemplate(i, showPokemonNumber, pokemonNameBigLetter, showPokemonImage, pokemonType) {
+    return `<div id="pokemonCard${i}" class="pokemonCard">
+                <span class="pokemonNumber"><b>${showPokemonNumber}</b></span>
+                <h1 id="pokemonNames">${pokemonNameBigLetter}</h1>
+                <img loading="lazy" id="pokemonImage" src= "${showPokemonImage}">
+                <div id="pokemonTypeContainer" class="pokemonTypeContainer">
+                    <span class="pokemonType">${pokemonType}</span>
+                </div>
+                <div class="background-image-Card">
+                </div>
+            </div>`
+}
+
+function typeIsTwoTemplate(i, showPokemonNumber, pokemonNameBigLetter, showPokemonImage, pokemonType) {
+    return `<div id="pokemonCard${i}" class="pokemonCard">
+                <span class="pokemonNumber"><b>${showPokemonNumber}</b></span>
+                <h1 id="pokemonNames">${pokemonNameBigLetter}</h1>
+                <img loading="lazy" id="pokemonImage" src= "${showPokemonImage}">
+                <div id="pokemonTypeContainer" class="pokemonTypeContainer">
+                    <span class="pokemonType">${pokemonType}</span>
+                    <span class="pokemonType">${loadedPokemon[i]['types']['1']['type']['name']}</span>
+                </div>
+                <div class="background-image-Card">
+                </div>
+            </div>`
+}
+
+function changeBackgroundColor(pokemonType) {
+    if (pokemonType.includes("grass")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-grass') }
+    else if (pokemonType.includes("fire")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-fire') }
+    else if (pokemonType.includes("water")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-water') }
+    else if (pokemonType.includes("bug")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-bug') }
+    else if (pokemonType.includes("normal")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-normal') }
+    else if (pokemonType.includes("poison")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-poison') }
+    else if (pokemonType.includes("electric")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-electric') }
+    else if (pokemonType.includes("ground")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-ground') }
+    else if (pokemonType.includes("fairy")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-fairy') }
+    else if (pokemonType.includes("fighting")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-fighting') }
+    else if (pokemonType.includes("psychic")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-psychic') }
+    else if (pokemonType.includes("rock")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-rock') }
+    else if (pokemonType.includes("ghost")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-ghost') }
+    else if (pokemonType.includes("ice")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-ice') }
+    else if (pokemonType.includes("dragon")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-dragon') }
+    else if (pokemonType.includes("dark")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-dark') }
+    else if (pokemonType.includes("steel")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-steel') }
+    else if (pokemonType.includes("flying")) { document.getElementById(`pokemonCard${i}`).classList.add('bg-flying') }
 }
